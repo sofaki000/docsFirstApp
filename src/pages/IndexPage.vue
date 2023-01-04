5389503e27a74585de238367d98ab614226e2648
sofaki000 <sofikardami2000@gmail.com>
2023-01-04 13:25:20 +0200

    everything

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn color="primary" label="Create incident" />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
---
+++ src/pages/violationsPage.vue
@@ -0,0 +1,1 @@
+<template><div>My violations</div></template>
