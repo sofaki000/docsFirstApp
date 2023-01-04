c4137063aa08f1cce601594748f9c480a535dd21
sofaki000 <sofikardami2000@gmail.com>
2023-01-04 12:45:26 +0200

    pages

---
+++ quasar-project/src/components/incidentsComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="q-pa-md" style="max-width: 350px">
+    <q-list dense bordered padding class="rounded-borders">
+      <q-item :key="item.name" v-for="item in incidents" clickable v-ripple>
+        <q-item-section> {{ item.name }} </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "EssentialLink",
+  props: {
+    incidents: {
+      type: Array,
+      required: true,
+    },
+  },
+});
+</script>
